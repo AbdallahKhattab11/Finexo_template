98bf6b58422a988c0a33b1f5d8ec2679d4c7bd65
Abdallah Khattab <freelancerabdallah@gmail.com>
2025-01-08 01:38:18 +0200

    added services page

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,7 +1,14 @@
+import Footer from "../../components/Footer/Footer"
+import Info from "../../components/Info/Info"
+import OurServices from "../../components/OurServices/OurServices"
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+      <OurServices />
+      <Info/>
+      <Footer/>
+    </div>
   )
 }
 
