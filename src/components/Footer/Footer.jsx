0512a83be628405ffaa486de129698f1efa90784
Abdallah Khattab <freelancerabdallah@gmail.com>
2025-01-08 01:32:42 +0200

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+  return (
+    <footer className="py-[10px] elementCenter">
+      <p className="flex items-center gap-2 px-[20px] text-center">
+      &copy;
+      2025/1/8 All Rights Reserved by :
+        <a className="font-semibold text-primary-bg" href="https://www.linkedin.com/in/abdallah-khattab-653729247/">Abdallah Khattab</a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import WhyChooseUs from "../../components/WhyChooseUs/WhyChooseUs";
 import OurTeam from "../../components/OurTeam/OurTeam";
 import WhatSaysOurCustomers from "../../components/WhatSaysOurCustomers/WhatSaysOurCustomers";
 import Info from "../../components/Info/Info";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -18,6 +19,7 @@ const Home = () => {
       <OurTeam />
       <WhatSaysOurCustomers />
       <Info/>
+      <Footer/>
     </section>
   );
 };
