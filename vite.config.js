c1a3742bd3c1912b1aa1d9a1a223d7282324baf1
Moamal-2000 <moamalalaapro1@gmail.com>
2025-01-09 16:59:15 +0300

    fix: use vite-plugin-compression

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import compression from 'compression'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    compression({ algorithm: 'gzip' }),
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
   ],
-})
-
-
+});
