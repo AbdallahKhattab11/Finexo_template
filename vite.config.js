c3d6992577c5eca9f2918d3ca51eacefc9ffb2ae
Abdallah Khattab <freelancerabdallah@gmail.com>
2025-01-09 19:40:22 +0200

    Fix build issues

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import compression from 'compression'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    compression({ algorithm: 'gzip' }),
   ],
 })
 
